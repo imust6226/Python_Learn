10a671fa024476ad4a6b052f03e062835bd08618
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-11 16:14:09 +0800

    导入类
    从模块导入单个类；
    从模块导入多个类
    导入多个模块的类

---
+++ 《Python编程从入门到实践》/09-类/eg_my_cars.py
@@ -0,0 +1,8 @@
+from eg_car import Car
+from eg_electric_car2 import ElectricCar
+
+my_beetle = Car("volkswagen", "beetle", "2016")
+print(my_beetle.get_descriptive_name())
+
+my_tesla = ElectricCar("tesla", "roadster", 2016)
+print(my_tesla.get_descriptive_name())
