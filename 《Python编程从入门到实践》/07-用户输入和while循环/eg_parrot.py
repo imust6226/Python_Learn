451c6d0f51e14db265cf4083cde7b34a615d20af
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-14 09:35:49 +0800

    input()
    使用input()函数 接受用户的输入数据。

---
+++ 《Python编程从入门到实践》/07-用户输入和while循环/eg_parrot.py
@@ -0,0 +1,16 @@
+#coding = utf-8
+
+message = input("Tell me something,and I will repeat it back to you:")
+print(message)
+
+
+name = input("Please enter your name:")
+print("Hello, " + name + "!")
+
+
+prompt = "If you tell us who you are,we can personalize the messages you see."
+prompt += "\nWhat is your first name?"
+name = input(prompt)
+print("Hello, " + name + "!")
+
+
