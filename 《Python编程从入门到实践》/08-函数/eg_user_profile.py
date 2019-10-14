9268e0418596d62d8561e02a098f75e15619c7a0
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-14 16:43:20 +0800

    任意数量的关键字实参

---
+++ 《Python编程从入门到实践》/08-函数/eg_user_profile.py
@@ -0,0 +1,26 @@
+#coding = utf-8
+
+"""使用任意数量的关键字实参调用函数"""
+
+def build_profile(first, last, **user_info):
+    """创建一个字典，其中包含我们知道的有关用户的一切"""
+    profile = {}
+    profile['first'] = first
+    profile['last'] = last
+
+    for key, value in user_info.items():
+        profile[key] = value
+
+    return profile
+
+user_profile1 = build_profile('albert', 'einstein', location = 'princeton',
+                             fielf = 'physics')
+print (user_profile1)
+
+
+others = {'sex':'female', 'age':28, 'hobby':'Love to Sing!'}
+
+#任意数量的关键字实参需要指定变量名哟，最好有意义，能够读明白是指的什么意思
+user_profile2 = build_profile('lcuy', 'mark', user_info = others)
+print(user_profile2)
+
