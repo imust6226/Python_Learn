916452021ff5acf6015b036da3aba078bd9620f4
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-15 17:36:45 +0800

    设置一个标志位，True或者False，来循环

---
+++ 《Python编程从入门到实践》/07-用户输入和while循环/eg_parrot2.py
@@ -0,0 +1,27 @@
+#coding = utf-8
+
+
+
+
+prompt = "\nTell me something ,and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+message = ''
+"""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print("Hello, " + message + "!")
+
+"""
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        
+        active = False
+    else:
+        print(message)
+
+
