7f2de36a9228c26c54f1db4d19c50e5fe5ce13fb
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-17 16:16:42 +0800

    Add files via upload

---
+++ 《Python编程从入门到实践》/03-列表简介/eg_bicycles.py
@@ -0,0 +1,10 @@
+#coding = utf-8
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+
+# 索引从0开始的
+print(bicycles[0].title())
+
+print("My first bicycle was a " + bicycles[0].title() + ".")
