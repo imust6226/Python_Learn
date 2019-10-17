c8b10474333ab0af6f897f3d1f6e86b9ff53bb5b
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-17 15:55:27 +0800

    完成习题

---
+++ 《Python编程从入门到实践》/02-变量和简单数据类型/work_noble_person.py
@@ -0,0 +1,17 @@
+#coding = utf-8
+
+famous_person = 'ablert einstein'
+msg = "A person who never made a mistake never tried anything new."
+print(famous_person + " once said,“" + msg + "”" )
+
+
+print(famous_person.title() + " once said,“" + msg + "”" )
+
+
+
+name = ' lily lucy '
+print("\t" + name + "\n")
+print("\n" + name + "\t")
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
