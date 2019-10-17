250dd8f1e74e08b1c5c808ea777e80db6f3896c6
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-17 16:23:38 +0800

    完成题目

---
+++ 《Python编程从入门到实践》/02-变量和简单数据类型/work_3_1.py
@@ -0,0 +1,20 @@
+#coding = utf-8
+
+names = ['zhao shun', 'qian shu', 'li mu', 'wang xie']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+
+
+print()
+
+words = " Nice to meet you!"
+print(names[0].title() + words)
+print(names[1].title() + words)
+print(names[2].title() + words)
+print(names[3].title() + words)
+
+
+vicle = ['honda motorcyle', 'volo', 'bmw', 'nisa']
+print("I would like to own a " + vicle[0].title() + ".")
