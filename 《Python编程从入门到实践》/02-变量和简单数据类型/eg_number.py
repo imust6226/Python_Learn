5fc8635b28f9693329de15b35704b5d322da062e
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-17 16:04:05 +0800

    数学运算

---
+++ 《Python编程从入门到实践》/02-变量和简单数据类型/eg_number.py
@@ -0,0 +1,45 @@
+#coding = utf-8
+
+a = 3 + 5
+print(a)
+
+b = 3 - 2
+print(b)
+
+c = 2 * 3
+print(c)
+
+d = 3 / 2
+print(d)
+
+e = 3 ** 2
+print(e)
+
+f = 3 ** 3
+print(f)
+
+g = 10 ** 6
+print(g)
+
+h = 2 + 3 * 4
+print(h)
+
+i = (2 + 3) * 4
+print(i)
+
+j = 0.1 + 0.1
+print(j)
+
+k = 2 * 0.1
+print(k)
+
+m = 0.2 + 0.1
+print(m)
+
+n = 3 * 0.1
+print(n)
+
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
