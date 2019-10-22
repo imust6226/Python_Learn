e460d3f774dfffa86386145444b3803cb82a9dd6
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-22 17:17:09 +0800

    列表的索引

---
+++ 《Python编程从入门到实践》/03-列表简介/eg_index.py
@@ -0,0 +1,13 @@
+#coding = utf-8
+
+motorcyles = ['honda', 'yamaha', 'suzuki']
+
+# 索引下表从0开始
+print(motorcyles[1])
+
+
+# 索引可以从 -1 开始，表示倒数第一个
+print(motorcyles[-1])
+
+"""如果列表元素是空的，使用索引可能出错，最好的方式
+就是使用len()函数打印出列表的长度"""
