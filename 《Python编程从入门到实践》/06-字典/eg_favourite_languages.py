57a877528e62e8fbe01237e8b164759f82534c36
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-30 11:29:20 +0800

    Add files via upload

--- 《Python编程从入门到实践》/06-字典/eg_favourite_languages.py
+++ 《Python编程从入门到实践》/06-字典/eg_favourite_languages.py
@@ -8,3 +8,30 @@ favourite_language = {
     }
 print("Sarah's favourite languages is " +
        favourite_language['sarah'].title() + '.')
+
+
+
+for name,language in favourite_language.items():
+    print("\n" + name.title() + "'s favourite language is " +
+          language.title() + ".")
+
+# 遍历所有的键
+
+friends = ['phil', 'sarah']
+
+for name in favourite_language.keys():
+    print("\n" + name.title())
+    if name in friends:
+        print("Hi," + name.title() +
+              ",I see your favourite languages is " +
+              favourite_language[name].title() + ".")
+    if 'erin' not in favourite_language.keys():
+        print("Erin, please take your poll!")
+    else:
+        print(favourite_language['erin'].title())
+
+
+# 遍历所有的值
+print("The following languages have been mentioned:")
+for language in favourite_language.values():
+    print(language.title())
---
+++ 《Python编程从入门到实践》/06-字典/eg_user.py
@@ -0,0 +1,12 @@
+#coding = utf-8
+
+# 遍历字典
+user_0 ={
+    'username':'efermi',
+    'first':'enrico',
+    'last':'fermi'
+    }
+
+for key,value in user_0.items():
+    print('\nKey:' + key)
+    print('Value:' + value)
