a25490bd848f8947d068ecef1b4e2950c3897157
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-30 11:12:43 +0800

    Add files via upload

---
+++ 《Python编程从入门到实践》/06-字典/eg_favourite_languages.py
@@ -0,0 +1,10 @@
+#coding = utf-8
+
+favourite_language = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python'
+    }
+print("Sarah's favourite languages is " +
+       favourite_language['sarah'].title() + '.')
