d8a2abf81840562f7100ca31650b31b69ebcf40e
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-30 10:03:38 +0800

    一系列独立的if语句

---
+++ 《Python编程从入门到实践》/05-if语句/5.3 if语句/eg_toppings_if.py
@@ -0,0 +1,15 @@
+#coding = utf-8
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("Finished making your pizza!")
+
+
+"""如果只想执行一个代码块就用 if-elif-else 结果，否则就用一些系列独立
+的if语句"""
