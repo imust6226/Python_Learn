d12f551ccb51b5b735fd36cf6ab01fa0c014bd4e
imust6226 <42436911+imust6226@users.noreply.github.com>
2019-10-30 09:48:25 +0800

    if-else语句

---
+++ 《Python编程从入门到实践》/05-if语句/5.3 if语句/eg_voting.py
@@ -0,0 +1,12 @@
+#coding = utf-8
+
+age = 19
+if age >= 18:
+    print("You are old enough to vote.")
+    print("Have you registered to vote yet?")
+
+else :
+    print("Sorry,You are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
+age = 17
